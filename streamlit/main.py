a8bb9d7150a21341cdd3313878948bb62926bcda
name <email@email.com>
2023-08-15 19:11:18 -0300

    fix: main

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from annotated_text import annotated_text
 
 hide_menu_style = """
         <style>
@@ -9,18 +8,4 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
-st.write('Hello, *World!* :sunglasses:')
-annotated_text(
-    "This ",
-    ("is", "verb"),
-    " some ",
-    ("annotated", "adj"),
-    ("text", "noun"),
-    " for those of ",
-    ("you", "pronoun"),
-    " who ",
-    ("like", "verb"),
-    " this sort of ",
-    ("thing", "noun"),
-    "."
-)
+
