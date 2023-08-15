0b255cc1bb1d36c73853fe1bc1f1cd5ada9b19f9
name <email@email.com>
2023-08-15 18:12:21 -0300

    add streamlit

---
+++ streamlit/main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from annotated_text import annotated_text
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.write('Hello, *World!* :sunglasses:')
+annotated_text(
+    "This ",
+    ("is", "verb"),
+    " some ",
+    ("annotated", "adj"),
+    ("text", "noun"),
+    " for those of ",
+    ("you", "pronoun"),
+    " who ",
+    ("like", "verb"),
+    " this sort of ",
+    ("thing", "noun"),
+    "."
+)
