4018004d759285a3968c583390e8745218f443e4
name <email@email.com>
2023-08-23 22:03:33 -0300

    add: tag

--- taguette/static/js/taguette.js
+++ taguette/static/js/taguette.js
@@ -885,7 +885,7 @@ function updateTagsList() {
     '</svg>'+
     '    <span class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-blue-700  border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900" id="tag-' + tag.id + '-count">' + tag.count + '</span>' +
     
-      '  <div class="tag-name">' +
+      '  <div class="tag-name text-sm text-gray-900 dark:text-white">' +
       '    <a id="tag-link-' + tag.id + '">' + escapeHtml(tag.path) + '</a>' +
       '  </div>' +
        '    <a href="javascript:editTag(' + tag.id + ');" class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">' + gettext("Edit Tag") + '</a>' +
