e3a76adf721bad693cb2b984cbbb58067a84f5de
name <email@email.com>
2023-08-25 14:14:48 -0300

    add: tag

--- streamlit/main.py
+++ streamlit/main.py
@@ -11,3 +11,6 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.code(""" http://localhost:8501/ """)
+input = st.text_input('URL', 'https://docs.google.com/document/d/1VYT6uk7Q3DVAPudb1c7qIbojfoN3d5qgmH3ggREHmOo/edit?usp=sharing')
+st.code(""" https://docs.google.com/document/d/1EFGk8gaQwCqsPY-i5YaMTo41EVRgrhLs/edit?usp=sharing&ouid=100322575540100494079&rtpof=true&sd=true """)
